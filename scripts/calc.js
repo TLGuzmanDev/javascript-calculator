1aec76cc73b20f748231193b384f3e6c6ecdb2e8
Tony G <tlguzman94@gmail.com>
2020-04-15 14:33:02 -0400

    calc functionality

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,6 @@
-var displayValue = '';
-var operand1 = '';
+var displayValue = '0';
+var previousDisplay = '';
+var operand1 = '0';
 var operand2 = '';
 var operator = '';
 
@@ -30,7 +31,7 @@ function operate(op, x, y) {
             return subtract(x,y);
         case 'x':
             return multiply(x,y);
-        case '/':
+        case '÷':
             if (y == 0) {
                 return NaN;
             } else {
@@ -38,26 +39,87 @@ function operate(op, x, y) {
             }
         default:
             console.log("Error! invalid input recieved: " + op);
+            return NaN;
     }
 }
 
 function updateDisplay() {
-    document.getElementById('display').innerHTML = displayValue;
+    if (displayValue.includes('NaN')) {
+        clearDisplay();
+        displayValue = 'Error!';
+    }
+    document.getElementById('main-display').innerHTML = displayValue;
+    document.getElementById('second-display').innerHTML = previousDisplay;
 }
 
 function clearDisplay() {
-    document.getElementById('display').innerHTML = '';
-    displayValue = '';
-    operand1 = '';
+    document.getElementById('main-display').innerHTML = '0';
+    document.getElementById('second-display').innerHTML = '';
+    displayValue = '0';
+    previousDisplay = '';
+    operand1 = '0';
     operand2 = '';
     operator = '';
+}
+
+function deleteChar() {
+    if (displayValue != '0') {
+        if (operand2 != '') {
+            operand2 = operand2.substring(0, operand2.length - 1);
+            displayValue = operand1 + operator + operand2;
+        } else if (operator != '') {
+            if (operator == '=') {
+                operand1 = operand1.substring(0, operand1.length - 1);
+                operator = '';
+                displayValue = operand1;
+            } else {
+                operator = '';
+                displayValue = operand1;
+            }
+        } else {
+            operand1 = operand1.substring(0, operand1.length - 1);
+            if (operand1 == '-') {
+                operand1 = '';
+            }
+            displayValue = operand1;
+        }
+    }
+    updateDisplay();
+}
+
+function negative() {
+    if (displayValue != '') {
+        if (operand2 != '') {
+            operand2 = operate('x', '-1', operand2).toString();
+            displayValue = operand1 + operator + operand2;
+        } else if (operator != '') {
+            if (operator == '=') {
+                operand1 = operate('x', '-1', operand1).toString();
+                displayValue = operand1;
+            } else {
+                operand2 = '-';
+                displayValue = operand1 + operator + operand2;
+            }
+        } else {
+            operand1 = operate('x', '-1', operand1).toString();
+            displayValue = operand1;
+        }
+    } else {
+        operand1 = '-0';
+        displayValue = operand1;
+    }
+    updateDisplay();
 }
 
 function addOperand(element) {
     input = element.innerHTML;
     if (displayValue == '') {
         // case 1: display is empty
-        operand1 += input;
+        if (input == '.') {
+            operand1 = '0' + input;
+        } else {
+            operand1 = input;
+        }
         displayValue = operand1;
     } else if (operator == '') { 
         // case 2: operator hasnt been clicked.
@@ -65,10 +127,21 @@ function addOperand(element) {
         // check for multiple '.' in 1st operand 
         if(input == '.' && operand1.includes('.')) {
             clearDisplay();
-            displayValue = "Error!";
+            displayValue = "Error! Use only one decimal";
         } else {
-            operand1 += input;
-            displayValue = operand1
+            if(operand1 == '-0' && input != '.') {
+                operand1 = '-';
+                operand1 += input;
+            } else if (operand1 == '0') {
+                if (input == '.') {
+                    operand1 += input;
+                } else {
+                    operand1 = input;
+                }
+            } else {
+                operand1 += input;
+            }
+            displayValue = operand1;
         }
     } else {
         // case 3: operator selected
@@ -76,13 +149,21 @@ function addOperand(element) {
         // check for for multiple '.' in 2nd operand
         if (operator == '=') {
             clearDisplay();
-            operand1 = input;
+            if (input == '.') {
+                operand1 = '0' + input;
+            } else {
+                operand1 = input;
+            }
             displayValue = operand1;
         } else if(input == '.' && operand2.includes('.')) {
             clearDisplay();
-            displayValue = "Error!";
+            displayValue = "Error! Use only one decimal";
         } else {
-            operand2 += input;
+            if (input == '.' && operand2 == '') {
+                operand2 = '0' + input;
+            } else {
+                operand2 += input;
+            }
             displayValue = operand1 + operator + operand2;
         }
     }
@@ -91,34 +172,41 @@ function addOperand(element) {
 
 function addOperator(element) {
     input = element.innerHTML;
-    if (operand1 == '') {
+    if (operand1 == '' || operand1 == '.' || operand1 == '-.') {
         // case 1: no operand input
         clearDisplay();
-        displayValue = "Error!";
-    } else if (operator == '') {
+        displayValue = "Error! Enter value first";
+    } else if (operator == '' || operator == '=') {
         // case 2: operand1 input with no operator
         // check for invalid equals input
         if (input == '=') {
-            clearDisplay();
-            displayValue = "Error!";
+            if (operator == ''){
+
+            } else if (operator == '=') {
+
+            } else {
+                clearDisplay();
+                displayValue = "Error! Enter second value";
+            }
         } else {
             operator = input;
             displayValue = operand1 + operator;
         }
     } else {
         // case 3: eval equation
-        if(operand2 == '') {
+        if(operand2 == '' || operand2 == '-' || operand2 == '.') {
             clearDisplay();
-            displayValue = "Error!";
+            displayValue = "Error! Enter second value";
         } else {
+            previousDisplay = displayValue;
             operand1 = operate(operator, operand1, operand2).toString();
             if (operand1 == 'NaN') {
                 clearDisplay();
-                displayValue = "Error!";
+                displayValue = "Error! Divided by 0";
             } else {
                 operator = input;
                 operand2 = '';
-                if (input != '=') {
+                if (operator != '=') {
                     displayValue = operand1 + operator;
                 } else {
                     displayValue = operand1;
